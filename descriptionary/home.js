232e06e706e7ad655d20c4f2ee8f7293a9dfc224
bogyshi <avanroi1@binghamton.edu>
2016-02-13 23:50:07 -0500

    more updates

---
+++ descriptionary/home.js
@@ -0,0 +1,8 @@
+function clickDescibe(){
+    document.getElementById("home").display="none";
+    document.getElementById("describer").display="block";
+}
+function clickDescibe(){
+    document.getElementById("home").display="none";
+    document.getElementById("answerer").display="block";
+}
